b51b5d701aa81878b44170cb72fdb520d05f78c4
Viktor Sjölind <viktor@sjolind.se>
2017-06-14 19:16:49 +0200

    Refactor communication code to one file
    Signed-off-by: Viktor Sjölind <viktor@sjolind.se>

--- paph_server/src/Main.hs
+++ paph_server/src/Main.hs
@@ -1,8 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import Message
-import Data
 import ServerState
+import ClientHandling
 
 import Data.Aeson
 import Control.Exception (finally)
@@ -25,56 +24,3 @@ application state pending = do
     flip finally (disconnect client state) $ do
        connect client state
        clientHandler client state
-
--- | Waits until a specific message is recieved on the given connection.
-waitForMessage :: Message -> WS.Connection -> IO ()
-waitForMessage msgMatch conn = do
-    putStrLn $ "Waiting for a " ++ show msgMatch
-    jsonMsg <- WS.receiveData conn
-
-    let (Just recievedMsg) = decode jsonMsg
-    unless (msgMatch == recievedMsg) $ do
-        putStrLn $ "Ignoring unexpected msg: " ++ show recievedMsg
-        waitForMessage msgMatch conn
-
--- | Connect a client to the server
-connect :: Client -> MVar ServerState -> IO ()
-connect client@(vid, conn) state = do
-    WS.sendTextData conn (encode (Connect vid))
-    print $ "A user has claimed slot " ++ show vid
-    liftIO $ modifyMVar_ state $ \s ->
-        return $ addClient client s
-
--- | Disconnect a client from the server
-disconnect :: Client -> MVar ServerState -> IO ()
-disconnect client state = do
-    modifyMVar state $ \s ->  let s' = removeClient client s in return (s', s')
-    putStrLn $ show (fst client) ++ " has disconnected"
-
--- | Handles requests for a client.
--- Loops until a disconnect message is recieved.
-clientHandler :: Client -> MVar ServerState -> IO ()
-clientHandler client@(_, conn) state = do
-    jsonMsg <- WS.receiveData conn
-    case decode jsonMsg of
-        Just Disconnect -> return ()
-        Just msg -> do
-            liftIO $ readMVar state >>= broadcast msg
-            clientHandler client state
-        Nothing -> clientHandler client state
-
-
--- | Perform a handshake using a specific connection
-handshake :: WS.Connection -> MVar ServerState -> IO Client
-handshake conn state = do
-    -- Wait for a Ready message and then respond with available spots
-    waitForMessage Ready conn
-    s <- liftIO $ readMVar state
-    WS.sendTextData conn (encode (Available (available s)))
-
-    -- Next message needs to be a Connect
-    jsonMsg <- WS.receiveData conn
-    case decode jsonMsg of
-        Just (Connect vid) -> return (vid, conn)
-                              -- TODO Check if client already connected
-        _                  -> error "Illegal handshake, Did not receive a Connect"
